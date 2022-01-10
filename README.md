bb51c833515aab5454ce3c94e96ad5496d6c371e
mateusz <mateusz@stake.fish>
2022-01-10 23:56:29 +0400

    initial

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # sf-fs
+
+All unit test are skipped so the failing test do not pollute your console output. You can use your favorite code editor.
+
+## Rules
+
+* The `create_employee` function is used by several services by very different teams within organization. The API must remain reverse-compatible.
+* Do NOT use external libraries.
+* Feel free to ask questions.
+
+## Tasks
+
+1. Our new intern wrote a controller responsible for creating new employees. Unfortunately, all unit tests are failing - try to fix them. 
+2. A senior software engineer complained about data validation. Validation rules seem to be duplicated, the code is not reusable and we want to use it for other controllers. It must be refactored - propose a refactoring plan.
+
+## How to run?
+
+```
+$ npm install
+$ npm test
+```
---
+++ tasks/01_create_employee.js
@@ -0,0 +1,53 @@
+const default_values = {
+  first_name: '',
+  last_name: '',
+  created_at: null,
+  last_modified: null,
+  history: [{
+    position: '',
+    salary: NaN,
+    current: true
+  }]
+}
+
+/**
+ * Creates a new employee.
+ * 
+ * @param {*} personal_data 
+ */
+function create_employee(personal_data) {
+  const employee = Object.assign(
+    {},
+    default_values,
+    {
+      created_at: new Date(),
+    },
+    personal_data,
+  );
+
+  if (employee.first_name.length === 0) {
+    throw new Error('You must provide first name to create a new employee');
+  }
+
+  if (employee.last_name.length === 0) {
+    throw new Error('You must provide last name to create a new employee');
+  }
+
+  if (employee.history[0].position.length === 0) {
+    throw new Error('You must assign position to create a new employee');
+  }
+
+  if (isNaN(employee.history[0].salary)) {
+    throw new Error('You must assign salary to create a new employee');
+  }
+
+  if (employee.history[0].current !== undefined) {
+    throw new Error('You must assign current position to create a new employee');
+  }
+
+  console.log('[INFO] The employee has been created');
+
+  return employee;
+}
+
+module.exports = create_employee;
